8da168f2ce53f53903e7c0b3a74bd9c6e54d7db9
dcbrown <browncdonovan@gmail.com>
2025-01-28 14:05:54 -0500

    increaase time out

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,19 @@ const nextConfig: NextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["fs"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
